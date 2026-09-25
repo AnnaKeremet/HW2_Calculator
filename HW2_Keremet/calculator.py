d53a5765714e3ff95dc7a61b8410332dd158a081
Shchepotin Roman <roman.shepotin@gmail.com>
2026-09-25 22:34:45 +0300

    Add subtract function

--- HW2_Keremet/calculator.py
+++ HW2_Keremet/calculator.py
@@ -1,18 +1,2 @@
-def main():
-    equation=input('ВВЕДИТЕ: ').split()
-    a=float(equation[0])
-    b=float(equation[2])
-    symbol=equation[1]
-    if symbol == "+":
-        print(add(a,b))
-    elif symbol == "-":
-        print(subtract(a,b))
-    elif symbol == "*":
-            print(multiply(a,b))
-    elif symbol == "/":
-            print(divide(a,b))
-    else:
-        print("Неверно выбрана математическая операция (поддерживается: +,-,*./). Попробуйте снова.")
-    
-if __name__ == "__main__":
-    main()    
+def subtract(a, b):
+    return a - b
